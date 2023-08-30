90a9e086f63eae52182c451fb72243d6ecef72dc
hussain <hussainint08@gmail.com>
2023-08-30 12:06:39 +0530

    last

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/2.10.3/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/admin/Desktop/flutterCode/offline_store_upload"
+export "FLUTTER_APPLICATION_PATH=/Users/admin/Desktop/flutterCode/packages/offline_store_upload"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=0.0.2"
+export "FLUTTER_BUILD_NUMBER=0.0.2"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
